55010b0f0e48832ca1c06b16ad9d407cdec5e905
rgithu <92245617+rgithu@users.noreply.github.com>
2023-01-12 17:02:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(310,310);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('myfilter.jpg');
+}
